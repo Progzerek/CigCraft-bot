c7acbbb1b451683d2a4eb7ee02cc4b96f604d899
Progzerek <nagybalint159@gmail.com>
2025-12-12 14:18:01 +0100

    Refactor PufferPanel integration and command structure
    Refactor PufferPanel authentication and improve error handling. Update commands for better clarity and functionality.

--- main.py
+++ main.py
@@ -7,32 +7,43 @@ import os
 from mcstatus import JavaServer
 import asyncio
 
+#load from env
 load_dotenv()
 PUFFER_MC_ID=os.getenv('PUFFER_MC_ID')
 MC_ID=os.getenv('MC_ID')
 PUFFER_URL = os.getenv('PUFFER_URL')
-PUFFER_USER = os.getenv('PUFFER_USER')
-PUFFER_PASS = os.getenv('PUFFER_PASS')
-token = os.getenv('DISCORD_TOKEN')
-
+CLIENT_ID= os.getenv('CLIENT_ID')
+CLIENT_SECRET=os.getenv('CLIENT_SECRET')
+Puffer_token = f"{PUFFER_URL}/oauth2/token"
+dc_token=os.getenv("DISCORD_TOKEN")
 dc_id = int(os.getenv('DC_ID'))
 
+#bot permissions
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
 def get_puffer_token():
-    url = os.getenv('TOKEN')
-    data = {
-        "grant_type": "client_credentials",
-        "client_id": os.getenv('CLIENT_ID'),
-        "client_secret": os.getenv('CLIENT_SECRET')
-    }
-    response = requests.post(url, data=data)
-    return response.json().get("access_token")
-
+    try:
+        print("trying to connect")
+        url = Puffer_token
+        data = {
+            "grant_type": "client_credentials",
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET
+        }
+        response = requests.post(url, data=data)
+        access_token = response.json().get("access_token")
+        resp = response.json()
+        return access_token, resp
+    except:
+        print(f"Failed {response.status_code}: {response.text}")
+        return None, None
+#prefix
 mc_bot = commands.Bot(command_prefix='/mc ', intents=intents)
+
+#commands
 
 @mc_bot.event
 async def on_ready():
@@ -40,20 +51,23 @@ async def on_ready():
 
 @mc_bot.command()
 async def hello(ctx):
-    await ctx.send(f"Hello {ctx.author.mention}")
+    await ctx.send(f"Hello {ctx.author.mention}, wanna play minecraft?")
 
 @mc_bot.command()
 async def status(ctx):
 #puffer conect
-    token = get_puffer_token()
+    token, response = get_puffer_token()
+    if token is None:
+        return await ctx.send("**Error:** PufferPanel Auth Failed.")
     headers = {"Authorization": f"Bearer {token}"}
+
 #status true or false
     status = f"{PUFFER_URL}/daemon/server/{PUFFER_MC_ID}/status"
     response_status = requests.get(status, headers=headers)
     data_status = response_status.json()
     status_value = data_status.get('running', True)
     if status_value == True:
-#cpu and ram
+    #cpu and ram
         cpu_ram_stat = f"{PUFFER_URL}/daemon/server/{PUFFER_MC_ID}/stats"
         response_cpu_ram = requests.get(cpu_ram_stat, headers=headers)
         data_cpu_ram = response_cpu_ram.json()
@@ -62,8 +76,8 @@ async def status(ctx):
         raw_ram = data_cpu_ram.get('memory', 0)
         ram_gb = raw_ram / (1024 ** 3)
         ram = f"{ram_gb:.2f}"
-#player list, ping
-        if status_value == True:
+    #player list, ping
+        try:
             server = JavaServer.lookup(f"{MC_ID}") 
             status = server.status()
             if status.players.sample:
@@ -71,31 +85,35 @@ async def status(ctx):
                 player_list = "\n".join(names)
             else:
                 player_list = " - None"
+        except:
+            await ctx.send("Could not connect to the Minecraft server")
 
         await ctx.send(f" **Server Status**\n Status: `{status_value}`\n CPU: `{cpu}%`\n RAM: `{ram} GB`\n Players:\n`{player_list}`")
     elif status_value==False:
         await ctx.send("**Server offline**")
     else:
-        await ctx.sens("Error")
+        await ctx.send("Error")
 
 @mc_bot.command()
 async def start(ctx):
-    token = get_puffer_token()
+    token, response = get_puffer_token()
+    if token is None:
+        return await ctx.send("**Error:** PufferPanel Auth Failed.")
     headers = {"Authorization": f"Bearer {token}"}
+    
     status = f"{PUFFER_URL}/daemon/server/{PUFFER_MC_ID}/status"
     response_status = requests.get(status, headers=headers)
     data_status = response_status.json()
     status_value = data_status.get('running', True)
     if status_value == True:
-        await ctx.send ("**Server is already online**")
-        return
+        return await ctx.send ("**Server is already online**")
+ 
     #start signal
     url_start = f"{PUFFER_URL}/daemon/server/{PUFFER_MC_ID}/start?wait=false"
     resp = requests.post(url_start, headers=headers)
     
     if resp.status_code not in [202, 204]:
         return await ctx.send(f"Start failed: {resp.status_code}")
-        
     msg = await ctx.send("**Sending start command...**")
     
     #log check
@@ -133,19 +151,39 @@ async def start(ctx):
 
 @mc_bot.command()
 async def stop(ctx, *, force: str = "normal"):
-    token = get_puffer_token()
+    token, response = get_puffer_token()
+    if token is None:
+        return await ctx.send("**Error:** PufferPanel Auth Failed.")
     headers = {"Authorization": f"Bearer {token}"}
     
-    status = f"{PUFFER_URL}/daemon/server/{PUFFER_MC_ID}/status"
-    response_status = requests.get(status, headers=headers)
-    data_status = response_status.json()
-    status_value = data_status.get('running', True)
-    if status_value == False:
-        await ctx.send ("**Server is already offline**")
-        return
+    #error handling
+    if response not in [202, 204]:
+        return await ctx.send(f"Failed {response.status_code}: {response.text}")
 
-    server = JavaServer.lookup(f"{MC_ID}") 
-    status = server.status()
+    #check status
+    try:
+        status_url = f"{PUFFER_URL}/daemon/server/{PUFFER_MC_ID}/status"
+        response_status = requests.get(status_url, headers=headers)
+        
+        if response_status.status_code == 200:
+            data_status = response_status.json()
+            status_value = data_status.get('running')
+            
+            if status_value is False:
+                await ctx.send("**Server is already offline!**")
+                return
+    except:
+        await ctx.send("Warning: Failed to get server status from PufferPanel. Trying to stop anyway...")
+        pass
+        
+    #player lookup
+    try:
+        server = JavaServer.lookup(f"{MC_ID}")
+        status = server.status()
+    except:
+        return await ctx.send("Failed to connect to Minecraft")
+    
+    #player handling
     if status.players.sample:
         names = [p.name for p in status.players.sample]
         player_list = ", ".join(names)
@@ -154,21 +192,17 @@ async def stop(ctx, *, force: str = "normal"):
     
     if player_list != "none" and force=="force":
         if ctx.author.id != dc_id:
-            await ctx.send ("No permission dumass")
-            return
+            return await ctx.send ("No permission")
         else:
             msg = await ctx.send ("**Kicking out players...**")
     elif player_list != "none":
-        await ctx.send (f"**Players {player_list} are still online!**")
-        return
-    else:
-        pass
-    
+        return await ctx.send (f"**Players {player_list} are still online!**")
+        
     #stop signal
     url_stop = f"{PUFFER_URL}/daemon/server/{PUFFER_MC_ID}/stop?wait=false"
     resp = requests.post(url_stop, headers=headers)
     if force=="force":
-        await msg.edit(content="**Stopping Server...** (Saving world...)")
+        msg = await msg.edit (content="**Stopping Server...** (Saving world...)")
     else:
         msg = await ctx.send ("**Stopping Server...** (Saving world...)")
     
@@ -176,7 +210,8 @@ async def stop(ctx, *, force: str = "normal"):
     url_console = f"{PUFFER_URL}/daemon/server/{PUFFER_MC_ID}/console"
     start_time = asyncio.get_event_loop().time()
     while True:
-        await asyncio.sleep(2) # Check every 2s
+        await asyncio.sleep(2)
+        
         #Hard timeout - 3 mins
         if asyncio.get_event_loop().time() - start_time > 180:
              await msg.edit(content="**Stop timed out (3m)")
@@ -191,9 +226,7 @@ async def stop(ctx, *, force: str = "normal"):
                     full_log = log_resp.text
                 
                 if full_log:
-                    # Look at last ~1000 characters
                     snippet = full_log[-1000:]
-                    # Check for "Running post-execution steps"
                     if "Running post-execution steps" in snippet:
                         await msg.edit(content="**Server Stopped Successfully!**")
                         return
@@ -206,6 +239,7 @@ async def stop(ctx, *, force: str = "normal"):
 @mc_bot.command()
 async def helper(ctx):
     msg = """**Help Menu**
+`/mc helper` - Shows this list
 `/mc hello` - Greet the server
 `/mc start` - Start the server
 `/mc stop` - Stop the server if no players online
@@ -219,17 +253,20 @@ async def helper(ctx):
 @mc_bot.command()
 async def cmd(ctx, *, command_to_run):
     if ctx.author.id != dc_id:
-        return await ctx.send("No permission dumass")
+        return await ctx.send("No permission")
         
-    token = get_puffer_token()
+    token, response = get_puffer_token()
+    if token is None:
+        return await ctx.send("**Error:** PufferPanel Auth Failed.")
     headers = {
         "Authorization": f"Bearer {token}",
         "Content-Type": "text/plain" 
     }
-    url_console = f"{PUFFER_URL}/daemon/server/{PUFFER_MC_ID}/console"
-    response = requests.post(url_console, headers=headers, data=command_to_run)
     
-    if response.status_code == 204:
+    url_console = f"{PUFFER_URL}/daemon/server/{PUFFER_MC_ID}/console"
+    response_console = requests.post(url_console, headers=headers, data=command_to_run)
+    
+    if response_console.status_code == 204:
         await asyncio.sleep(0.5) 
         read_headers = {"Authorization": f"Bearer {token}"}
         log_resp = requests.get(url_console, headers=read_headers)
@@ -246,14 +283,17 @@ async def cmd(ctx, *, command_to_run):
                 output = f"Sent: `{command_to_run}`\nResponse: `{last_line}`"
         await ctx.send(output)
     else:
-        await ctx.send(f"Failed {response.status_code}: {response.text}")
+        await ctx.send(f"Failed {response_console.status_code}: {response_console.text}")
 
 @mc_bot.command()
 async def logs(ctx):
     if ctx.author.id != dc_id:
         return await ctx.send("No premission dumass")
-    token = get_puffer_token()
+    token, response = get_puffer_token()
+    if token is None:
+        return await ctx.send("**Error:** PufferPanel Auth Failed.")
     headers = {"Authorization": f"Bearer {token}"}
+    
     url = f"{PUFFER_URL}/daemon/server/{PUFFER_MC_ID}/console"
     resp = requests.get(url, headers=headers)
     if resp.status_code == 200:
@@ -274,38 +314,44 @@ async def logs(ctx):
 @mc_bot.command()
 @commands.cooldown(1, 1800, commands.BucketType.guild)
 async def timeout(ctx):
-    token = get_puffer_token()
+    token, response = get_puffer_token()
+    if token is None:
+        return await ctx.send("**Error:** PufferPanel Auth Failed.")
     headers = {
         "Authorization": f"Bearer {token}",
         "Content-Type": "text/plain" 
     }
+
     url = f"{PUFFER_URL}/daemon/server/{PUFFER_MC_ID}/console"
-    response = requests.post(url, headers=headers, data="gamerule pvp false")
-    if response.status_code == 204:
+    response_console = requests.post(url, headers=headers, data="gamerule pvp false")
+    if response_console.status_code == 204:
         await ctx.send(f"**PVP is off for 10 minutes**")
         url = f"{PUFFER_URL}/daemon/server/{PUFFER_MC_ID}/console"
-        response = requests.post(url, headers=headers, data="say PVP is inactive for 10 mins")
+        response_console = requests.post(url, headers=headers, data="say PVP is inactive for 10 mins")
         await asyncio.sleep(600)
 
-        token = get_puffer_token()
+        token, response = get_puffer_token()
+        if token is None:
+            return await ctx.send("**Error:** PufferPanel Auth Failed.")
         headers = {
             "Authorization": f"Bearer {token}",
             "Content-Type": "text/plain" 
         }
-        url = f"{PUFFER_URL}/daemon/server/{PUFFER_MC_ID}/console"
-        response = requests.post(url, headers=headers, data="gamerule pvp true")
-        url = f"{PUFFER_URL}/daemon/server/{PUFFER_MC_ID}/console"
-        response = requests.post(url, headers=headers, data="say PVP is now active")
-        @timeout.error
-        async def timeout_error(ctx, error):
-           if isinstance(error, commands.CommandOnCooldown):
-                total_seconds = int(error.retry_after)
-                minutes = total_seconds // 60
-                seconds = total_seconds % 60
         
-                await ctx.send(f"**Cooldown: `{minutes} minutes {seconds} seconds`.")
+        url = f"{PUFFER_URL}/daemon/server/{PUFFER_MC_ID}/console"
+        response_console = requests.post(url, headers=headers, data="gamerule pvp true")
+        url = f"{PUFFER_URL}/daemon/server/{PUFFER_MC_ID}/console"
+        response_console = requests.post(url, headers=headers, data="say PVP is now active")
+        
     else:
-        await ctx.send(f"Failed {response.status_code}: {response.text}")
-    
+        await ctx.send(f"Failed {response_console.status_code}: {response_console.text}")
 
-mc_bot.run(token, log_handler=handler, log_level=logging.DEBUG)
+@timeout.error
+async def timeout_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        total_seconds = int(error.retry_after)
+        minutes = total_seconds // 60
+        seconds = total_seconds % 60
+        await ctx.send(f"**Cooldown: `{minutes} minutes {seconds} seconds`.")    
+
+mc_bot.run(dc_token, log_handler=handler, log_level=logging.DEBUG)
